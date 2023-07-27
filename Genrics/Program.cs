b0a9b07e90d3c153f3ead2628dc8bac22bf6a896
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-07-27 20:35:48 +0530

    [sakshi]  uploading program.cs for calling an same array using generics

---
+++ Genrics/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Genrics
+{
+    class PrintArray
+    {
+        public static void ToPrint<T>(T[] arr)
+        {
+           foreach(var element in arr)
+            {
+                Console.WriteLine(element);
+            }
+           
+
+        }
+ 
+    }
+    class program
+    {    public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Genrics");
+            int[] intArray = { 10, 20, 30, 40 };
+            PrintArray.ToPrint<int>(intArray);
+            double[] doubleArray = { 1.5, 2.8, 3.0, 4.0 };
+            PrintArray.ToPrint<double>(doubleArray);
+            char [] charArray = { 'h', 'j', 'k','s'};
+            PrintArray.ToPrint<char>(charArray);
+        }
+    }   
+}
