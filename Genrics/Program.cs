198d3722de6e44f48f9fa88df49860cbb9fd65d6
sakshijadhav12 <sakshijjadhav28@gmail.com>
2023-07-28 23:50:27 +0530

    [sakshi] modifying program.cs to giving more than three paramerts

--- Genrics/Program.cs
+++ Genrics/Program.cs
@@ -4,88 +4,48 @@ namespace Generics
 {
     public class GenericMaximun<T> where T : IComparable
     {
-        public T firstValue, secondValue, thirdvalue;
-        public GenericMaximun(T firstValue, T secondValue, T thirdvalue)
+        public T[] value;
+
+        public GenericMaximun(T[] value)
         {
-            this.firstValue = firstValue;
-            this.secondValue = secondValue;
-            this.thirdvalue = thirdvalue;
+            this.value = value;
         }
 
-        public static T MaxValue(T firstValue, T secondValue, T thirdValue)
+        public T[] Sort(T[] values)
         {
-            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
-                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
-                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
-            {
-                return firstValue;
-            }
-            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
-                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
-                secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
-            {
-                return secondValue;
-            }
-            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
-                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
-                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0)
-            {
-                return thirdValue;
-            }
-            return default;
+            Array.Sort(values);
+            return values;
         }
 
-        public T MaxMethod()
+        public T MaxValue(params T[] values)
         {
-            T max = GenericMaximun<T>.MaxValue(this.firstValue, this.secondValue, this.thirdvalue);
+            var sorted_Values = Sort(values);
+            return sorted_Values[^1];
+        }
+
+        public T MaxMethod(params T[] values)
+        {
+            var max = MaxValue(this.value);
             return max;
         }
-    }
-    class Program
-    {
-        static void Main(string[] args)
+
+         public void PrintMaxValue()
+         {
+         var max = MaxValue(this.value);
+        Console.WriteLine("maximum value is " + max);
+        }
+        }
+
+        public class Program
         {
-           
-            Console.WriteLine("Enter first integer:");
-            int firstInt = Convert.ToInt32(Console.ReadLine());
+            public static void Main(string[] args)
+            {
+                int[] arr = { 23, 56, 456, 444, 88, 89 };
+                GenericMaximun<int> generic = new GenericMaximun<int>(arr);
+                generic.PrintMaxValue();
 
-            Console.WriteLine("Enter second integer:");
-            int secondInt = Convert.ToInt32(Console.ReadLine());
+              
 
-            Console.WriteLine("Enter third integer:");
-            int thirdInt = Convert.ToInt32(Console.ReadLine());
-
-            GenericMaximun<int> intMax = new GenericMaximun<int>(firstInt, secondInt, thirdInt);
-            int maxIntValue = intMax.MaxMethod();
-            Console.WriteLine("The maximum integer value is: " + maxIntValue);
-
-           
-            Console.WriteLine("Enter first double:");
-            double firstDouble = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter second double:");
-            double secondDouble = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter third double:");
-            double thirdDouble = Convert.ToDouble(Console.ReadLine());
-
-            GenericMaximun<double> doubleMax = new GenericMaximun<double>(firstDouble, secondDouble, thirdDouble);
-            double maxDoubleValue = doubleMax.MaxMethod();
-            Console.WriteLine("The maximum double value is: " + maxDoubleValue);
-
-            
-            Console.WriteLine("Enter first string:");
-            string firstString = Console.ReadLine();
-
-            Console.WriteLine("Enter second string:");
-            string secondString = Console.ReadLine();
-
-            Console.WriteLine("Enter third string:");
-            string thirdString = Console.ReadLine();
-
-            GenericMaximun<string> stringMax = new GenericMaximun<string>(firstString, secondString, thirdString);
-            string maxStringValue = stringMax.MaxMethod();
-            Console.WriteLine("The maximum string value is: " + maxStringValue);
+        }
         }
     }
-}
